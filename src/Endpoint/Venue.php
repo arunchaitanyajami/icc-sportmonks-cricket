a1267384157adc9f9d4190417825bf3c5769fde5
Arun Chaitanya jami <arun.chaitanya@ntsindia.co.in>
2022-05-19 01:07:03 +0530

    Updated venues endpoints

--- src/Endpoint/Venue.php
+++ src/Endpoint/Venue.php
@@ -2,6 +2,7 @@
 
 namespace ICC\Sportmonks\Cricket\Endpoint;
 
+use ICC\Sportmonks\Cricket\Endpoint;
 use ICC\Sportmonks\Cricket\Exception\ApiRequestException;
 use ICC\Sportmonks\Cricket\CricketClient;
 use stdClass;
@@ -10,27 +11,32 @@ use stdClass;
  * Class Venue
  * @package ICC\Sportmonks\Cricket\Endpoint
  */
-class Venue extends CricketClient
-{
-    /**
-     * @param int $venueId
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function getById(int $venueId)
-    {
-        $url = "venues/{$venueId}";
-        return $this->call($url);
-    }
+class Venue extends CricketClient implements Endpoint {
+	/**
+	 * @param int $venueId
+	 *
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getById( int $venueId ) {
+		$url = "venues/{$venueId}";
 
-    /**
-     * @param int $seasonId
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function getBySeasonId(int $seasonId)
-    {
-        $url = "venues/season/{$seasonId}";
-        return $this->call($url);
-    }
+		return $this->call( $url );
+	}
+
+	/**
+	 * @param int $seasonId
+	 *
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getBySeasonId( int $seasonId ) {
+		$url = "venues/season/{$seasonId}";
+
+		return $this->call( $url );
+	}
+
+	public function getAll( $args = [] ) {
+		return $this->call( 'venues' );
+	}
 }
