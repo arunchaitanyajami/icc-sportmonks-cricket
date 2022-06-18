3289c0cdf7837980ba29a43d21a359652efb96b9
Arun Chaitanya jami <arun.chaitanya@ntsindia.co.in>
2022-06-18 11:32:14 +0530

    Stage Api changes

--- src/Endpoint/Stage.php
+++ src/Endpoint/Stage.php
@@ -10,27 +10,42 @@ use stdClass;
  * Class Stage
  * @package ICC\Sportmonks\Cricket\Endpoint
  */
-class Stage extends CricketClient
-{
-    /**
-     * @param int $stageId
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function getById(int $stageId)
-    {
-        $url = "stages/{$stageId}";
-        return $this->call($url);
-    }
+class Stage extends CricketClient {
+	/**
+	 * @param int $stageId
+	 *
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getById( int $stageId ): stdClass {
+		$url = "stages/{$stageId}";
 
-    /**
-     * @param int $seasonId
-     * @return stdClass
-     * @throws ApiRequestException
-     */
-    public function getBySeasonId(int $seasonId)
-    {
-        $url = "stages/season/{$seasonId}";
-        return $this->call($url);
-    }
+		return $this->call( $url );
+	}
+
+	/**
+	 * @param array $args
+	 *
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getAll( array $args = [] ): stdClass {
+		if ( ! empty( $args['page'] ) ) {
+			$this->setPage( $args['page'] );
+		}
+
+		return $this->call( "fixtures" );
+	}
+
+	/**
+	 * @param int $seasonId
+	 *
+	 * @return stdClass
+	 * @throws ApiRequestException
+	 */
+	public function getBySeasonId( int $seasonId ): stdClass {
+		$url = "stages/season/{$seasonId}";
+
+		return $this->call( $url );
+	}
 }
